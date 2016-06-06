584bdf7e321bc48239466536ce64ada536313ae2
CentOS7 <apktoolOo@gmail.com>
2016-06-06 20:37:41 +0800

    二叉排序树|Binary Search Tree[BST]

--- 5.24.03.c
+++ 5.24.03.c
@@ -1,7 +1,7 @@
 //二叉排序树Binary Search Tree[BST]
 #include<stdio.h>
 #include<stdlib.h>
-#define MaxSize 10
+#define MaxSize 12
 typedef int ElemType;
 typedef struct BiTNode{
 	ElemType data;
@@ -11,37 +11,37 @@ typedef struct BiTNode{
 BiTNode* CreateBiTree(BiTNode*,ElemType*);
 void BST_Insert(BiTNode**,ElemType);
 int BST_Search(BiTNode*,ElemType);
+int BST_Delete(BiTNode*,ElemType);
+void BST_Judge(BiTNode*);
 
 void PreOrder(BiTNode*);
 void InOrder(BiTNode*);
 
 int main(int argc,char* argv[]){
+	printf(">> %s|%s|%d Complete\n",__FILE__,__FUNCTION__,__LINE__);
 	ElemType ch[MaxSize]={
-		4,2,5,1,3,6,-1
+		9,5,10,3,7,11,3,6,8,2,4,-1	
 	};
 
 	BiTNode* T=NULL;
 	T=CreateBiTree(T,ch);
 
-	PreOrder(T);
-	printf("\n");
-	InOrder(T);
-	printf("\n");
+	BST_Judge(T);
 
-	int flag;
-	flag=BST_Search(T,9);
-	if(!flag){
-		printf("Find\n");
-	}else{
-		printf("NO Find\n");
-	}
+	int flag,no=1;
+	
+	flag=BST_Search(T,no);
+	flag?printf("No Find %d, ",no):printf("Find %d, ",no);
+	flag=BST_Delete(T,no);
+	flag?printf("Delete Failed,Have no '%d'\n",no):printf("Delete Success\n");
 
-	printf("%s|%d Complete\n",__FUNCTION__,__LINE__);
+	BST_Judge(T);
 
 	return 0;
 }
 
 BiTNode* CreateBiTree(BiTNode* T,ElemType ch[]){
+	printf(">> %s|%s|%d Complete\n",__FILE__,__FUNCTION__,__LINE__);
 	for(int i=0;i<MaxSize&&ch[i]!=-1;i++){
 		BST_Insert(&T,ch[i]);
 	}
@@ -68,19 +68,91 @@ int BST_Search(BiTNode* T, ElemType ch){
 		return -1;
 	}
 	if(ch<T->data){
-		BST_Search(T->lchild,ch);
+		return BST_Search(T->lchild,ch);
 	}else if(ch>T->data){
-		BST_Search(T->rchild,ch);
+		return BST_Search(T->rchild,ch);
 	}else{
 		return 0;
 	}
+}
+
+int BST_Delete(BiTNode* T,ElemType ch){
+	BiTNode *p=T;
+	BiTNode *pre=NULL;
+
+	while(p->data!=ch){
+		if(p->lchild==NULL&&p->rchild==NULL){
+			return -1;
+		}
+
+		if(ch<p->data){
+			pre=p;
+			p=p->lchild;
+		}else if(ch>p->data){
+			pre=p;
+			p=p->rchild;
+		}else{
+			break;
+		}
+	}
+
+	//该节点为叶子结点
+	if(p->lchild==NULL&&p->rchild==NULL){
+		if(p->data<pre->data){
+			pre->lchild=NULL;
+		}else{
+			pre->rchild=NULL;
+		}
+		free(p);
+	}
+
+	//该节点只有左子树而没有右子树
+	if(p->lchild!=NULL&&p->rchild==NULL){
+		if(p->data<pre->data){
+			pre->lchild=p->lchild;
+		}else{
+			pre->rchild=p->lchild;
+		}
+		free(p);
+	}
+
+	//该节点只有右子树而没有左子树
+	if(p->lchild==NULL&&p->rchild!=NULL){
+		if(p->data<pre->data){
+			pre->lchild=p->rchild;
+		}else{
+			pre->rchild=p->rchild;
+		}
+		free(p);
+	}
+
+	//该节点同时拥有左右子树
+	if(p->lchild!=NULL&&p->rchild!=NULL){
+		BiTNode *r=p;
+		p=p->rchild;
+		while(p->lchild!=NULL){
+			pre=p;
+			p=p->lchild;
+		}
+		r->data=p->data;
+
+		if(p->data<pre->data){
+			pre->lchild=r->lchild;
+		}else{
+			pre->rchild=p->rchild;
+		}
+
+		free(p);
+	}
+
+	return 0;
 }
 
 void PreOrder(BiTNode* T){
 	if(T==NULL){
 		return;
 	}
-	printf("%d",T->data);
+	printf("%3d",T->data);
 	PreOrder(T->lchild);
 	PreOrder(T->rchild);
 }
@@ -90,6 +162,13 @@ void InOrder(BiTNode* T){
 		return;
 	}
 	InOrder(T->lchild);
-	printf("%d",T->data);
+	printf("%3d",T->data);
 	InOrder(T->rchild);
+}
+
+void BST_Judge(BiTNode* T){
+	PreOrder(T);
+	printf("\n");
+	InOrder(T);
+	printf("\n");
 }
