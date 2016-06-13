3a89d7ced3f9322dca5a97d151aa4f2c9f40de31
CentOS7 <apktoolOo@gmail.com>
2016-06-13 16:45:38 +0800

    二叉排序树|Binary Search Tree[BST]|递归插入，递归查找|莫名错误很多

--- 6.08.01.c
+++ 6.08.01.c
@@ -1,4 +1,4 @@
-//二叉排序树|Binary Search Tree[BST]
+//二叉排序树|Binary Search Tree[BST]|递归插入，查找
 #include<stdio.h>
 #include<stdlib.h>
 #define MaxSize 20
@@ -11,7 +11,7 @@ typedef struct BiTNode{
 
 BiTNode* BST_Create(BiTNode*,ElemType*);
 BiTNode* BST_Insert(BiTNode*,ElemType);
-int BST_Search(BiTNode*,ElemType);
+BiTNode* BST_Search(BiTNode*,ElemType);
 BiTNode* BST_Delete(BiTNode*,ElemType);
 
 void BST_Judge(BiTNode*);
@@ -20,31 +20,31 @@ void InOrder(BiTNode*);
 
 int main(int argc, char* argv[]){
 	ElemType ch[MaxSize]={
-		9,5,5,13,3,7,11,15,4,6,10,16,-1
+		9,5,13,3,7,11,15,4,6,10,16,-1
 	};
 
 	BiTNode* T=NULL;
 	T=BST_Create(T,ch);
-	
+
+	BiTNode* P=T;
 	BST_Judge(T);
 
 	ElemType num;
 
 	printf("Please input the number you will insert: ");
 	scanf("%d",&num);
-	T=BST_Insert(T,num);
-	T?printf("Insert Success\n"):printf("Illeged Insert\n");
+	P=BST_Insert(T,num);
+	P?printf("Insert Success\n"):printf("Illeged Insert\n");
 
-	int flag;
 	printf("Please input the number you will Find: ");
 	scanf("%d",&num);
-	flag=BST_Search(T,num);
-	flag?printf("Not Found\n"):printf("Find the number: %d\n",num);
+	P=BST_Search(T,num);
+	P?printf("Find the number: %d\n",num):printf("Not Found\n");
 
 	printf("Please input the number you will Delete: ");
 	scanf("%d",&num);
-	T=BST_Delete(T,num);
-	T?printf("Delete Success\n"):printf("illege Delete\n");
+	P=BST_Delete(T,num);
+	P?printf("Delete Success\n"):printf("illege Delete\n");
 
 	BST_Judge(T);
 	return 0;
@@ -62,36 +62,37 @@ BiTree BST_Create(BiTNode* T, ElemType ch[]){
 }
 
 BiTNode* BST_Insert(BiTNode* T,ElemType ch){
+	int flag=1;
 	if(T==NULL){
 		T=(BiTNode*)malloc(sizeof(BiTNode));
 		T->data=ch;
 		T->lchild=NULL;
 		T->rchild=NULL;
-	}else{
+		flag=0;
+	}
 		if(ch<T->data){
 			T->lchild=BST_Insert(T->lchild,ch);
 		}
 		if(ch>T->data){
 			T->rchild=BST_Insert(T->rchild,ch);
 		}
-		if(ch==T->data){
+		if(ch==T->data&&flag){
 			return NULL;
 		}
-	}
 
 	return T;
 }
 
-int BST_Search(BiTNode* T,ElemType ch){
+BiTNode* BST_Search(BiTNode* T,ElemType ch){
 	if(T==NULL){
-		return -1;
+		return NULL;
 	}
 	if(ch<T->data){
 		return BST_Search(T->lchild,ch);
 	}else if(ch>T->data){
 		return BST_Search(T->rchild,ch);
 	}else{
-		return 0;
+		return T;
 	}
 }
 
