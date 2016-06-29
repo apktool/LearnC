94482a6c4c01ce114f77d9873a5d44974a9a724f
CentOS7 <apktoolOo@gmail.com>
2016-06-29 15:08:21 +0800

    getpwuid(),getpwnam(),getgrgid(),getgrnam()

--- 6.29.04.c
+++ 6.29.04.c
@@ -1,29 +1,33 @@
-//getpwuid(),getpwnam(),getgrgid(),getgrnam()
+/**
+ * @file 6.29.04.c
+ * @brief getpwuid(),getpwnam(),getgrgid(),getgrnam()
+ * @author LiWenGang
+ * @date 2016-06-29
+ */
 #include<sys/types.h>
 #include<pwd.h>
 #include<grp.h>
 #include<stdio.h>
-#include<stdlib.h>
 
-char* userNameFromId(uid_t uid);	//通过UID获取用户名
+char* userNameFromId(uid_t uid);		//通过UID获取用户名
 uid_t userIdFromName(const char* name);	//通过用户名获取UID
-char* groupFromId(gid_t gid);		//通过gid获取组名
-gid_t groupIdFromName(const char* name);	//通过组名获取GID
+char* groupFromId(gid_t gid);			//通过gid获取组名
+gid_t groupIdFromName(const char* name);//通过组名获取GID
 
 int main(int argc,char* argv[]){
 	char* username;
 	uid_t uid=1000;	
+
 	username=userNameFromId(uid);
 	printf("username=%s\n",username);
-
 	uid=userIdFromName("li");
 	printf("uid=%d\n",uid);
 	
 	char* groupname;
 	gid_t gid=1000;
+	
 	groupname=groupFromId(gid);
 	printf("groupname=%s\n",groupname);
-
 	gid=groupIdFromName(groupname);
 	printf("gid=%d\n",gid);
 	
@@ -31,25 +35,25 @@ int main(int argc,char* argv[]){
 }
 
 char* userNameFromId(uid_t uid){
-	struct passwd *pwd;
-	pwd=getpwuid(uid);
-	if(pwd==NULL){
+	struct passwd *usr;
+	usr=getpwuid(uid);
+	if(usr==NULL){
 		return NULL;
 	}else{
-		return pwd->pw_name;
+		return usr->pw_name;
 	}
 }
 
 uid_t userIdFromName(const char* name){
-	struct passwd *pwd;
+	struct passwd *usr;
 	if(name==NULL||*name==0){
 		return -1;
 	}
-	pwd=getpwnam(name);
-	if(pwd==NULL){
+	usr=getpwnam(name);
+	if(usr==NULL){
 		return -1;
 	}
-	return pwd->pw_uid;
+	return usr->pw_uid;
 }
 
 char* groupFromId(gid_t gid){
@@ -73,5 +77,4 @@ gid_t groupIdFromName(const char* name){
 	}else{
 		return grp->gr_gid;
 	}
-
 }
