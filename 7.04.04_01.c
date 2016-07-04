c307450231fcd99520c8e7f72b43d11f51369532
CentOS7 <apktoolOo@gmail.com>
2016-07-04 16:47:59 +0800

    execve() 配合7.04.04_02.c使用

---
+++ 7.04.04_01.c
@@ -0,0 +1,19 @@
+/**
+ * @file 7.04.04_01.c
+ * @brief execve() 配合7.04.04_02.c使用
+ * @author LiWenGang
+ * @date 2016-07-04
+ */
+#include<stdio.h>
+
+int main(int argc, char* argv[]){
+	for(int i=0;i<argc;i++){
+		printf("argv[%d]:%s\n",i,argv[i]);
+	}
+	return 0;
+}
+
+/* 
+ * gcc 7.04.04_02.c -o temp -Wall -std=c11
+ * ./temp2 ./temp1
+ */
