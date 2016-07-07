4ea1302b683ac9c0cd8d90dcb25a7e54cc2741e6
CentOS7 <apktoolOo@gmail.com>
2016-07-07 10:55:38 +0800

    pthread_creat(),pthread_join()

--- 7.07.01.c
+++ 7.07.01.c
@@ -4,35 +4,35 @@
  * @author LiWenGang
  * @date 2016-07-07
  */
+
 #include<sys/types.h>
 #include<pthread.h>
-#include<unistd.h>
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 
-static void* threadFunc(void* arg);
+static void* pthreadFun(void*);
 
 int main(int argc, char* argv[]){
-	pthread_t t1;
-	void *res;
-	int s;
+	pthread_t thd;
+	void* s;
+	int flag;
 
-	s=pthread_create(&t1,NULL,threadFunc,"Hello world\n");
-	if(s!=0){
-		perror("error pthread_creat");
+	flag=pthread_create(&thd,NULL,pthreadFun,"hello world");
+	if(flag!=0){
+		perror("error pthread create");
 	}
-	printf("Message from main()\n");
-	s=pthread_join(t1,&res);
-	if(s!=0){
-		perror("error pthread_join");
+	printf("this message from main()\n");
+	flag=pthread_join(thd,&s);
+	if(flag!=0){
+		perror("error pthread join");
 	}
-	printf("Thread returned %ld\n",(long)res);
+	printf("%ld\n",(long int)s);
 	return 0;
 }
 
-static void* threadFunc(void* arg){
-	char* s=(char*)arg;
-	printf("%s",s);
-	return (void*)strlen(s);
+static void* pthreadFun(void* s){
+	char* str=(char*)s;
+	puts(str);
+	return (void*)strlen(str);
 }
