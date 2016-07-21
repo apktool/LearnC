58604196f432549648b69a448485cd8dcc803d1b
CentOS7 <apktoolOo@gmail.com>
2016-07-21 10:45:07 +0800

    ftp|execute_command()未完成

--- 7.21.01.c
+++ 7.21.01.c
@@ -1,5 +1,8 @@
 #include<sys/types.h>
+#include<sys/stat.h>
+#include<unistd.h>
 #include<signal.h>
+#include<fcntl.h>
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -40,7 +43,33 @@ void shell_loop(void);
 void init(void);
 int read_command(void);
 int parse_command(void);
+int execute_command(void);
+void get_command(int);
 int check(const char* str);
+
+void do_exit(void);
+void do_cd(void);
+void do_type(void);
+void do_about(void);
+void do_jobs(void);
+void do_kill(void);
+
+typedef void (*cmd_handler)(void);
+typedef struct builtin_cmd{
+	char* name;
+	cmd_handler handler;
+}builtin_cmd_t;
+builtin_cmd_t builtins[]={
+	{ "exit",do_exit },
+	{ "cd",do_cd },
+	{ "type",do_type },
+	{ "about",do_about },
+	{ "jobs",do_jobs },
+	{ "kill",do_kill },
+	{ NULL,NULL }
+};
+
+int buildtin(void);
 
 int main(int argc, char* argv[]){
 	head=(node_t*)malloc(sizeof(node_t));
@@ -100,6 +129,59 @@ int parse_command(void){
 	}
 }
 
+int execute_command(void){
+	if(cmd_count==0){
+		return 0;
+	}
+	if(infile[0]!='\0'){
+		cmd[0].infd=open(infile,O_RDONLY);
+	}
+	if(outfile[0]!='\0'){
+		if(append){
+
+		}
+	}
+	return 0;
+}
+
+void get_command(int i){
+	int j=0;
+	int inword;
+	while(*lineptr==' '||*lineptr=='\t'){
+		lineptr++;
+	}
+	cmd[i].args[j]=avptr;
+	while(*lineptr!='\0'
+			&& *lineptr!=' '
+			&& *lineptr!='\t'
+			&& *lineptr!='>'
+			&& *lineptr!='<'
+			&& *lineptr!='|'
+			&& *lineptr!='&'
+			&& *lineptr!='\n'){
+		*avptr++=*lineptr++;
+		inword=1;
+	}
+	*avptr++='\0';
+	switch(*lineptr){
+		case ' ':
+		case '\t':
+			inword=0;
+			j++;
+			break;
+		case '<':
+		case '>':
+		case '|':
+		case '&':
+		case '\n':
+			if(inword==0){
+				cmd[i].args[j]=NULL;
+			}
+			return;
+		default:return;
+	}
+}
+
 int check(const char* str){
 	char* p;
 	while(*lineptr==' '||*lineptr=='\t'){
@@ -115,4 +197,50 @@ int check(const char* str){
 		return 1;
 	}
 	return 0;
+}
+
+int buildtin(void){
+	int i=0;
+	int found=0;
+	while(builtins[i].name!=NULL){
+		if(check(builtins[i].name)){
+			builtins[i].handler();
+			found=1;
+			break;
+		}
+		i++;
+	}
+	return found;
+}
+
+void do_exit(void){
+	int Pnum=0;
+	node_t* tmp=head->next;
+	while(tmp!=NULL){
+		Pnum++;
+		tmp=tmp->next;
+	}
+	if(Pnum!=0){
+		printf("There are programs in the background,are you sure to exit?y/N\n");
+		char c= getchar();
+		if(c=='N')
+			return ;
+		 else
+			goto loop;
+	}
+	loop:
+	printf("exit\n");
+	exit(EXIT_SUCCESS);
+}
+
+void do_cd(void){
+	get_command(0);
+	int fd;
+	fd=open(*(cmd[0].args),O_RDONLY);
+	fchdir(fd);
+	close(fd);
+}
+
+void do_about(void){
+	printf("copyright@Mr'Li 2016-7-21");
 }
