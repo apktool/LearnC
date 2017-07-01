8ef1f3600c7b85051a799d62bccaa95d5b525c8f
apktool <apktoolOo@gmai.com>
2017-07-01 11:32:00 +0800

    数字移位操作

---
+++ 2017/7.01.01.c
@@ -0,0 +1,15 @@
+/**
+ * @file 7.01.01.c
+ * @Synopsis  数字移位操作
+ * @author apktool
+ * @version 0.1
+ * @date 2017-07-01
+ */
+
+#include<stdio.h>
+
+int main(int argc, char* argv[]){
+	unsigned int memory = 1 << 20;
+	printf("%X\n", memory);
+	return 0;
+}
